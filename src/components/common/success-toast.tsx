e60f48956f706c5ae8b0b66364585531d281987e
yogz <yogzgo@gmail.com>
2025-12-29 17:01:07 +0100

    style: Increase z-index of success toast component.

--- src/components/common/success-toast.tsx
+++ src/components/common/success-toast.tsx
@@ -22,7 +22,7 @@ export function SuccessToast({
           initial={{ opacity: 0, y: -20, scale: 0.9 }}
           animate={{ opacity: 1, y: 0, scale: 1 }}
           exit={{ opacity: 0, y: -20, scale: 0.9 }}
-          className="pointer-events-none fixed inset-x-0 top-24 z-50 flex justify-center px-4"
+          className="pointer-events-none fixed inset-x-0 top-24 z-[100] flex justify-center px-4"
         >
           <div
             className={clsx(
